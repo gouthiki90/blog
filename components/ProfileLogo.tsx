5ea85b6acc327c257b8393a5aac1a69ba0ef639a
Jeon-Geaeun <snsdkf6@gmail.com>
2023-10-27 15:48:47 +0900

    init : 로고 작업 중

---
+++ components/ProfileLogo.tsx
@@ -0,0 +1,11 @@
+export default function BlogLogo() {
+  return (
+    <>
+      <a href="#">
+        <div className="m-10 flex justify-center">
+          <div className="profile-image blog-logo" />
+        </div>
+      </a>
+    </>
+  );
+}
