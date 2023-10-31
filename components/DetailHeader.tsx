a0cfd6b7dd3f1b58ef6d5f8f6cf5d560e7aff572
Jeon-Geaeun <snsdkf6@gmail.com>
2023-10-31 10:33:57 +0900

    Feat : JSON SERVER 추가

--- components/DetailHeader.tsx
+++ components/DetailHeader.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import MenuList from "./MenuList";
 import BlogLogo from "./ProfileLogo";
 import { AiOutlineMinus } from "react-icons/ai";
-import { Noto_Sans_KR } from "next/font/google";
+import { BsArrowClockwise } from "react-icons/bs";
+import { Nanum_Myeongjo } from "next/font/google";
 
-const notoSansKR = Noto_Sans_KR({ weight: "300", subsets: ["latin"] });
+const nanumMyengjo = Nanum_Myeongjo({ weight: "400", subsets: ["latin"] });
 
 export default function DetailHeader() {
   return (
     <>
+      <div className="flex flex-row-reverse m-1 text-[#71717a]">
+        <BsArrowClockwise className="ml-1" />
+        <span className={`${nanumMyengjo.className} text-xs`}>
+          2023년 10월 27일 5시 14분 업데이트
+        </span>
+      </div>
       <div className="flex justify-center flex-col mr-5">
         {/* 로고 이미지 */}
         <BlogLogo />
